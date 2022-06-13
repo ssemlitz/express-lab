a9029582e8392acd608d7721b3e3586e87fc6738
ssemlitz <sdsemlitz@yahoo.com>
2022-06-13 17:24:08 -0400

    functioning list of students

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Beryl', smart: true, _id: 127904},
+  {name: 'Jordin', smart: true, _id: 139608},
+  {name: 'Scott', smart: true, _id: 125223},
+]
+
+export { 
+	students
+}
