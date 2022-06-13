975aba07740ee12b49bbf57fce4c73b57fe5f934
ssemlitz <sdsemlitz@yahoo.com>
2022-06-13 18:01:32 -0400

    add cards for student data

--- data/student-data.js
+++ data/student-data.js
@@ -1,7 +1,7 @@
 const students = [
-  {name: 'Beryl', smart: true, _id: 127904},
-  {name: 'Jordin', smart: true, _id: 139608},
-  {name: 'Scott', smart: true, _id: 125223},
+  {name: 'Beryl', smart: true, description: 'Beryl is a super talented full-stack developer who enjoys singing and going on hot girl walks.', _id: 127904},
+  {name: 'Jordin', smart: true, description: 'Jordin is an incredibly talented full-stack developer who loves Shrek 2 and throwing games in Overwatch.', _id: 139608},
+  {name: 'Scott', smart: true, description: 'Scott is the smortest person in da whole world.', _id: 125223},
 ]
 
 export { 
--- server.js
+++ server.js
@@ -32,3 +32,19 @@ app.get('/students', function(req, res) {
 app.listen(3000, function() {
   console.log('Listening on port 3000')
 })
+
+
+
+
+
+
+
+{/* <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div> */}
